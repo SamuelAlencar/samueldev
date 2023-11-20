a475204bb77d84acb12c5748cbef1c6a851155cc
samuel_alencar <samuel_alencar@carrefour.com>
2023-11-20 17:09:43 -0300

    ajuste 456

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "photos.app.goo.gl",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "photos.app.goo.gl",
+  //     },
+  //   ],
+  // },
+  // experimental: {
+  //   serverActions: true,
+  // },
 };
 
 module.exports = nextConfig;
