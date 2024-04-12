b800d96e8373e123c89e247f37c74049012415b3
samuel_alencar <samuel_alencar@carrefour.com>
2024-04-12 14:00:13 -0300

    removing unused props

--- components/projects.tsx
+++ components/projects.tsx
@@ -7,19 +7,7 @@ import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
 
 
-export interface ProjectsProps {
-  ref: React.RefObject<HTMLDivElement>;
-  inView: boolean;
-
-  setActiveSection: (sectionName: string) => void;
-  timeOfLastClick: number;
-  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
-
-  activeSection: string;
-  index: number;
-
-  sectionInView: boolean;
-}
+export interface ProjectsProps {}
 
 export default function Projects(props: ProjectsProps) {
   const { ref } = useSectionInView("Projetos", 0.5);
