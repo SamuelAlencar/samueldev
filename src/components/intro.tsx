dcf9efc43cf77a22b02934282bfbad43fb26ca18
samuel_alencar <samuel_alencar@carrefour.com>
2024-05-13 15:16:02 -0300

    add folder restructuring fix-01

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
-import { useSectionInView } from "../lib/hooks";
+import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "../context/active-section-context";
 import { FaWhatsapp } from "react-icons/fa6";
 
